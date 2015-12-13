601b55d6335d994bb7807df2259c78829242f1c4
Eric Ma <ericmajinglong@gmail.com>
2015-12-13 22:34:20 +0000

    added digits.py

---
+++ digits.py
@@ -0,0 +1,37 @@
+X = [255, 0, 0]  # Red
+O = [0, 0, 0]  # Black
+G = [0, 255, 0] # Green
+
+blank = [
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O
+    ]
+
+
+one = [
+    O, O, O, O, G, O, O, O,
+    O, O, O, G, G, O, O, O,
+    O, O, O, O, G, O, O, O,
+    O, O, O, O, G, O, O, O,
+    O, O, O, O, G, O, O, O,
+    O, O, O, O, G, O, O, O,
+    O, O, O, O, G, O, O, O,
+    O, O, O, G, G, G, O, O
+    ]
+
+two = [
+    O, O, O, G, G, O, O, O,
+    O, O, G, O, O, G, O, O,
+    O, O, O, O, O, G, O, O,
+    O, O, O, O, O, G, O, O,
+    O, O, O, O, O, G, O, O,
+    O, O, O, O, G, O, O, O,
+    O, O, O, G, O, O, O, O,
+    O, O, G, G, G, G, O, O
+    ]
